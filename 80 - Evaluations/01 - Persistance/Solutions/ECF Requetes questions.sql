f5b9c2c7a83ad92861dc0cd5cf2ee70c28b2a5e3
AFPA1701871 <martine.poix@afpa.fr>
2023-10-31 15:55:55 +0100

    31/10/2023 15:55:54,71

---
+++ 80 - Evaluations/01 - Persistance/Solutions/ECF Requetes questions.sql
@@ -0,0 +1,16 @@
+1. Obtenir la liste des villes qui ont un nom d''au moins 40 lettres
+2. Obtenir la liste des départements d’outres-mer, c’est-à-dire ceux dont le numéro de département commence par “97”
+3. Obtenir la liste des départements  des Hauts-de-France trier par ordre alphabétique des noms de département (sans jointure)
+4. Obtenir le nom de toutes les villes, ainsi que le nom du département associé.
+# Les plus peuplées en 2012 apparaitront en premier
+5. Obtenir la liste du nom de chaque département, associé à son code et du nombre de commune au sein de ces départements, en triant afin d’obtenir en priorité les départements qui possèdent le plus de communes
+6. Obtenir la liste des départements, classés en fonction de leur superficie (plus grand en premier)
+7. Compter le nombre de villes dont le nom commence par “Saint”
+8. Obtenir la liste des villes qui ont un nom existants plusieurs fois, et trier afin d’obtenir en premier celles dont le nom est le plus souvent utilisé par plusieurs communes
+9. Obtenir en une seule requête SQL la liste des villes dont la superficie est supérieure à la superficie moyenne
+10. Obtenir la liste des départements qui possèdent plus de 1.5 millions d’habitants en 2012
+11. Remplacez les tirets par un espace vide, pour toutes les villes commençant par “SAINT-” (dans la colonne qui contient les noms en majuscule)
+12. Supplémentaire. Obtenir la liste des 50 villes ayant la plus faible superficie
+13. Ajouter une colonne region_nbDepartement dans la table regions (mettre le code dans le script de réponse)
+14. Ecrire une procédure stockée, qui vient mettre à jour cette colonne
+15. Créer une vue qui regroupe les 3 tables
