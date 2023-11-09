0244d1d5716251a1875ae7fd94e87ec74f4e8733
AFPA1701871 <martine.poix@afpa.fr>
2023-11-09 12:10:04 +0100

    09/11/2023 12:10:03,66

---
+++ 05 - Composants d'interface/2 - Développer en C#/1 - C# CLI/Exemples/ConfusionGenre/ConfusionGenre/Agences.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfusionGenre
+{
+    public class Agence
+    {
+        public string Nom { get; set; }
+        public int NbPC { get; set; }
+        public static int NbAgence { get; set; } = 0;
+
+        public Agence()
+        {
+            Nom = "test";
+            NbPC = 0;
+            NbAgence++;
+        }
+    }
+}
---
+++ 05 - Composants d'interface/2 - Développer en C#/1 - C# CLI/Exemples/ConfusionGenre/ConfusionGenre/Employe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfusionGenre
+{
+    public class Employe
+    {
+        public  string  Nom { get; set; }
+        public Agence AgenceEmploye { get; set; }
+
+        public override string  ToString ()
+        {
+            return Nom + AgenceEmploye + AgenceEmploye.NbPC + Agence.NbAgence;
+        }
+    }
+}
