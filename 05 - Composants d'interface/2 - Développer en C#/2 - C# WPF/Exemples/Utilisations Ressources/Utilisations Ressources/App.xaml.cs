f343aef8187239f28a37c8c3efd3316e55fa56e3
AFPA1701871 <martine.poix@afpa.fr>
2023-11-14 15:36:46 +0100

    Utilisation Ressources

--- 05 - Composants d'interface/2 - Développer en C#/2 - C# WPF/Exemples/Utilisations Ressources/Utilisations Ressources/App.xaml.cs
+++ 05 - Composants d'interface/2 - Développer en C#/2 - C# WPF/Exemples/Utilisations Ressources/Utilisations Ressources/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Utilisation_Ressources
+namespace Utilisations_Ressources
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- 05 - Composants d'interface/2 - Développer en C#/2 - C# WPF/Exemples/Utilisations Ressources/Utilisations Ressources/MainWindow.xaml.cs
+++ 05 - Composants d'interface/2 - Développer en C#/2 - C# WPF/Exemples/Utilisations Ressources/Utilisations Ressources/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Utilisation_Ressources
+namespace Utilisations_Ressources
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
