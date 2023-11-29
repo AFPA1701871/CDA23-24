1b8f6b68f3282dbc29e1b365cc338db365c1164b
AFPA1701871 <martine.poix@afpa.fr>
2023-11-29 11:10:49 +0100

    ModelToBase

---
+++ 05 - Composants d'interface/3 - Accès aux données/4 - Solution/ModelToBase/ModelToBase/Models/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModelToBase.Models.Data;
+
+namespace ModelToBase.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Personnes> Personnes { get; set; }
+    }
+}
