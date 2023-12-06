7577cf2b73804ed53ba4baab7b89c7bf41096db2
AFPA1701871 <martine.poix@afpa.fr>
2023-12-06 11:17:01 +0100

    DAOJson : valable qqsoit le type d'objet

---
+++ 05 - Composants d'interface/3 - Accès aux données/4 - Solution/CRUD_JSON/CRUD_JSON/DAOJson.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_JSON
+{
+    public class DAOJson
+    {
+
+
+        //Méthodes
+
+        static public void EcrireFichier(List<Produits> liste, string path)
+        {
+            string[] contenu = { JsonConvert.SerializeObject(liste) };
+            File.WriteAllLines(path, contenu);
+        }
+        static public List<Object> LireFichier(string path)
+        //Renvoi un tableau de chaine contenant les records stockées dans le fichier records
+        {
+            string contenu;
+            List<Object> liste = new List<Object>();
+            //Lecture et stockage 
+            contenu = File.ReadAllText(path);
+            //transformation en liste d'object
+            liste = JsonConvert.DeserializeObject<List<Object>>(contenu);
+            return liste; // renvoi la liste des tableaux
+        }
+    }
+
+}
---
+++ 05 - Composants d'interface/3 - Accès aux données/4 - Solution/CRUD_JSON/CRUD_JSON/ProduitsServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_JSON
+{
+    public class ProduitsServices
+    {
+        static public string Path { get; set; } = "../../../Produits.json";
+
+        static public List<Produits> GetAllProduits()
+        {
+            List<Produits> liste = Profiles.FromObjectToProduits( DAOJson.LireFichier(Path));
+
+            return liste;
+        }
+    }
+}
